922cc409add1a0ebda1f62112b989eaeb6626587
Zbigniew Baranski <zb.baranski@icloud.com>
2019-12-21 00:41:16 +0100

    added styles

--- src/App.js
+++ src/App.js
@@ -9,37 +9,52 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken')
 
   useEffect(() => {
     getRecipes();
-  },[])
+    // eslint-disable-next-line
+  },[query])
   // empty array above is a 2nd argument of useEffect - this means it will run only once
 
+  // async await fetch call for query and setRecipes update
   const getRecipes = async () => {
-    const response = await fetch( `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch( `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
   }
 
+  // clean way to use onChange in input
   const updateSearch = e => {
     setSearch(e.target.value);
   }
 
+  // preventDefault to stop page refresh on form submit
+  // update the setQuery state with search, this is to submit and effectively fetch input field text
+  // after pressing search button, not after every character
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
   return (
     <div className="App">
-      <form className="search-form">
+      <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button " type="submit">Search</button>
       </form>
-      {recipes.map(recipe => (
-        <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          calories={Math.round(recipe.recipe.calories)}
-          image={recipe.recipe.image}
-          servings={recipe.recipe.yield} />
-      ))}
+      <div className="recipes" >
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.url}
+            title={recipe.recipe.label}
+            calories={Math.round(recipe.recipe.calories)}
+            image={recipe.recipe.image}
+            servings={recipe.recipe.yield}
+            ingredients={recipe.recipe.ingredients} />
+        ))}
+      </div>
     </div>
   );
 }
